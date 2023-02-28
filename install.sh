e0123a401b825a23cdb83629ee85224b8a9fefbb
kazuki kondo <k.kazuki526@icloud.com>
2023-02-28 10:51:24 +0900

    install.shを削除

--- install.sh
+++
@@ -1,8 +0,0 @@
-code --install-extension sporto.rails-go-to-spec
-code --install-extension mechatroner.rainbow-csv
-code --install-extension shardulm94.trailing-spaces
-code --install-extension aki77.rails-db-schema
-code --install-extension christian-kohler.path-intellisense
-code --install-extension oderwat.indent-rainbow
-code --install-extension donjayamanne.githistory
-code --install-extension streetsidesoftware.code-spell-checker
